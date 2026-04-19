6e241eb565c3deab97fa31228513cbe79942041e
Matthew Cania <matthewgab24@gmail.com>
2026-04-19 20:30:51 +0800

    Fixed Resume Download

--- src/partials/Header.jsx
+++ src/partials/Header.jsx
@@ -1,31 +1,32 @@
-
 export default function Header({ scrollToSection, navigation, isInView }) {
-	const isWhiteText = isInView["welcomeContent"] || isInView["projectsContent"];
+  const isWhiteText = isInView["welcomeContent"] || isInView["projectsContent"];
 
-	return (
-		<header className='flex justify-center lg:justify-end items-center w-full lg:w-3/4 h-10 gap-2 bg-transparent overflow-hidden z-2 fixed'>
-			{navigation.map((item, index) => (
-				<button
-					key={index}
-					onClick={() => scrollToSection(item.id)}
-					className={`font-thin flex justify-center items-center w-auto px-4 rounded-lg cursor-pointer text-sm outline-none
-						${isWhiteText ? 'text-white' : 'text-black'}
-						${isWhiteText ? 'hover:bg-white hover:text-zinc-950' : 'hover:bg-zinc-950 hover:text-white'}`}
-					style={{ transition: 'color 500ms ease, background-color 500ms ease' }}
-				>
-					{item.label}
-				</button>
-			))}
-			<a 
-				className={`font-thin flex justify-center items-center w-auto px-4 rounded-lg cursor-pointer text-sm outline-none
-					${isWhiteText ? 'text-white' : 'text-black'}
-					${isWhiteText ? 'hover:bg-white hover:text-zinc-950' : 'hover:bg-zinc-950 hover:text-white'}`}
-				style={{ transition: 'color 500ms ease, background-color 500ms ease' }}
-				href="Portfolio-Website/Matthew Cania Resume.pdf"
-				download="Matthew Cania Resume.pdf"
-			>
-				Resume
-			</a>
-		</header>
-	);
+  return (
+    <header className='flex justify-center lg:justify-end items-center w-full lg:w-3/4 h-10 gap-2 bg-transparent overflow-hidden z-2 fixed'>
+      {navigation.map((item, index) => (
+        <button
+          key={index}
+          onClick={() => scrollToSection(item.id)}
+          className={`font-thin flex justify-center items-center w-auto px-4 rounded-lg cursor-pointer text-sm outline-none
+						${isWhiteText ? "text-white" : "text-black"}
+						${isWhiteText ? "hover:bg-white hover:text-zinc-950" : "hover:bg-zinc-950 hover:text-white"}`}
+          style={{
+            transition: "color 500ms ease, background-color 500ms ease",
+          }}
+        >
+          {item.label}
+        </button>
+      ))}
+      <a
+        className={`font-thin flex justify-center items-center w-auto px-4 rounded-lg cursor-pointer text-sm outline-none
+    ${isWhiteText ? "text-white" : "text-black"}
+    ${isWhiteText ? "hover:bg-white hover:text-zinc-950" : "hover:bg-zinc-950 hover:text-white"}`}
+        style={{ transition: "color 500ms ease, background-color 500ms ease" }}
+        href={"http://localhost:3173/Portfolio-Website/Matthew_Gabriel_M_Cania-Resume.pdf"}
+        download
+      >
+        Resume
+      </a>
+    </header>
+  );
 }
